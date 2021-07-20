353e2ea9b78058eb6738ebb06ef5780131065684
Philip <philipbdev@gmail.com>
2021-07-20 18:00:27 -0400

    Fixed

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,4 +1,4 @@
 export default process.env.NODE_ENV === 'development'
-  ? 'http://localhost:5000'
+  ? 'https://city-weather-app-1.herokuapp.com'
   : process.env.NODE_ENV === 'production' &&
     'https://city-weather-app-1.herokuapp.com';
