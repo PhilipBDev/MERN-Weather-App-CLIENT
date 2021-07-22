36aea1d3627428c950e733135f7650146f67f782
Philip <philipbdev@gmail.com>
2021-07-22 15:06:16 -0400

    Adjusted domain

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,3 +1,3 @@
 export default process.env.NODE_ENV === 'development'
   ? 'http://localhost:5000'
-  : process.env.NODE_ENV === 'production' && 'https://api.myweather.city';
+  : process.env.NODE_ENV === 'production' && 'http://api.myweather.city';
